192f9efe541f4b4bb1ab0901666785122f324d96
Jay Doane <jay.s.doane@gmail.com>
2016-06-18 13:07:23 -0700

    Send header and receive response
    Current attempts by the client to close a socket for writing, as a
    trigger for the server to send a response, fail because ssl:shutdown/2
    apparently doesn't really support such a feature, in spite of the
    documentation.
    
    This allows the sender to prepend the file with a header containing the
    expected file size, which the receiver can then use to determine when it
    should stop reading from the socket, and so trigger a response. The
    receiver determines whether the stream contains a header by examining
    the first 4 bytes for a "magic" byte sequence. Currently, only the
    'filesize' key is required, whose value must be a positive integer
    matching the byte count of the file.
    
    The response may contain potentially useful information such as the
    absolute path of the remote file, received file size, and checksum.

--- src/netcp.hrl
+++ src/netcp.hrl
@@ -1,11 +1,17 @@
--define(DEFAULT_TRANSPORT, gen_tcp).
+-define(DEFAULT_TRANSPORT, ssl).
 -define(DEFAULT_PORT, 11011).
 
+-define(MAGIC, <<"«º«º«º«º">>).
+
 -define(BASE_TCP_OPTS, [
-    binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
+    binary, {packet, 0}, {active, false}, {reuseaddr, true}, {exit_on_close, false}]).
+-define(RECV_TIMEOUT, 1000). % ms
+-define(RESPONSE_TIMEOUT, 5000). % ms
 
 -define(DEFAULT_RECBUF_SIZE, 16*1024*1024).
 -define(DEFAULT_FILE_READ_BUF_SIZE, 16*1024*1024).
+-define(BITS_PER_BYTE, 8).
+-define(ENCODED_SIZE, 4).
 
 -define(DEFAULT_CERTFILE,
         filename:join([os:getenv("HOME"), ".certs", "certificate.pem"])).
