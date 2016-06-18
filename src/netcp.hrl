3ca5eac6ebfb424c1ca74868c335089992398f98
Jay Doane <jay.s.doane@gmail.com>
2016-06-18 12:49:21 -0700

    Improve flexibility of certificate location

--- src/netcp.hrl
+++ src/netcp.hrl
@@ -7,8 +7,10 @@
 -define(DEFAULT_RECBUF_SIZE, 16*1024*1024).
 -define(DEFAULT_FILE_READ_BUF_SIZE, 16*1024*1024).
 
--define(DEFAULT_CERTFILE, "/tmp/certificate.pem").
--define(DEFAULT_KEYFILE, "/tmp/key.pem").
+-define(DEFAULT_CERTFILE,
+        filename:join([os:getenv("HOME"), ".certs", "certificate.pem"])).
+-define(DEFAULT_KEYFILE,
+        filename:join([os:getenv("HOME"), ".certs", "key.pem"])).
 -define(DEFAULT_CIPHERS, [
     {rsa,aes_128_cbc,sha},
     {rsa,rc4_128,md5}]).
