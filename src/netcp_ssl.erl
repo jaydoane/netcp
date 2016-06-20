2d2d28440f02f300d064637c1246c98ab9a90156
Jay Doane <jay.s.doane@gmail.com>
2016-06-20 01:27:11 -0700

    Separate starting ssl from generating opts
    Create default cert on app start if it doesn't exist

---
+++ src/netcp_ssl.erl
@@ -0,0 +1,39 @@
+-module(netcp_ssl).
+
+-export([maybe_start_ssl/1, maybe_generate_default_cert/1]).
+
+-include("netcp.hrl").
+
+maybe_start_ssl(Opts) ->
+    case proplists:get_value(ciphers, Opts) of
+        undefined ->
+            ok;
+        _ ->
+            ok = ssl:start()
+    end.
+
+maybe_generate_default_cert(Opts) ->
+    case proplists:get_value(certfile, Opts) of
+        undefined ->
+            ok;
+        Path ->
+            case {Path =:= ?DEFAULT_CERTFILE, filelib:is_file(Path)} of
+                {true, false} ->
+                    ok = generate_cert();
+                _ ->
+                    ok
+            end
+    end.
+                    
+generate_cert() ->
+    generate_cert(?DEFAULT_KEYFILE, ?DEFAULT_CERTFILE).
+
+generate_cert(Keyfile, Certfile) ->
+    [ok = filelib:ensure_dir(Path) || Path <- [Keyfile, Certfile]],
+    Cmd = io_lib:format(
+        "openssl req -x509 -nodes -days 3650 "
+        "-subj '/C=US/ST=CA/O=NetCP Inc/CN=netcp.example.com' "
+        "-newkey rsa:2048 -keyout ~s -out ~s", [Keyfile, Certfile]),
+    Output = os:cmd(Cmd),
+    ok = io:format(Output).
+
