3bbaf05c7677edcaf5315721022d392880c09b33
Jay Doane <jay.s.doane@gmail.com>
2016-12-13 22:07:49 -0800

    Log warning rather than crash when port is in use
    This enables netcp to start on each node of a dev environment

--- src/netcp_app.erl
+++ src/netcp_app.erl
@@ -12,15 +12,21 @@ start(_StartType, _StartArgs) ->
     ListenOpts = listen_opts(Transport),
     ok = netcp_ssl:maybe_start_ssl(ListenOpts),
     ok = netcp_ssl:maybe_generate_default_cert(ListenOpts),
-    {ok, Listen} = Transport:listen(Port, ListenOpts),
-    netcp_log:info("netcp listening on port ~p transport ~p ~nopts ~p",
-        [Port, Transport, ListenOpts]),
-    case netcp_sup:start_link(Listen) of
-        {ok, Pid} ->
-            {ok, _} = netcp_sup:start_child(),
-            {ok, Pid, Listen};
-        Other ->
-            {error, Other}
+    case Transport:listen(Port, ListenOpts) of
+        {ok, Listen} ->
+            netcp_log:info("netcp listening on port ~p transport ~p ~nopts ~p",
+                [Port, Transport, ListenOpts]),
+            case netcp_sup:start_link(Listen) of
+                {ok, Pid} ->
+                    {ok, _} = netcp_sup:start_child(),
+                    {ok, Pid, Listen};
+                Other ->
+                    {error, Other}
+            end;
+        {error, eaddrinuse} ->
+            %% don't crash if e.g. development environment
+            netcp_log:warning("netcp port ~p in use", [Port]),
+            {ok, self()}
     end.
 
 stop(Listen) ->
